d9e4d86f6000187683a12016492c5c9e782c7edc
Daniel <daniel.marques@edu.unifil.br>
2023-03-07 09:12:02 -0300

    sealed-reacord, string template, pattern matching, testes

---
+++ testes/src/main/java/org/example/Pessoa.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+public class Pessoa {
+
+    String nome;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ testes/src/main/java/patternMatching/Main.java
@@ -0,0 +1,28 @@
+package patternMatching;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static Double convertWithoutPatterMaching(Object obj){
+        if(obj instanceof Integer){
+            return 1.0;
+        } else if(obj instanceof String){
+            return 2.0;
+        }else{
+            throw new RuntimeException();
+        }
+    }
+    public static Double convertWithPatterMaching(Object obj){
+        return switch(obj) {
+            case String s -> Double.parseDouble(s);
+            case Integer i -> i.doubleValue();
+            // case Integer i && i>10 -> i.doubleValue(); é como um If
+            // case Integer i -> 5; é como um else
+            // O mais genérico coloca em baixo
+            case null -> 2d;
+            default -> 0d;
+        };
+    }
+}

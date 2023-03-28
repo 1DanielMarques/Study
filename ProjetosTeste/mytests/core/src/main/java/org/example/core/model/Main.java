f3c5cdc8e1ed0248ea8bc423ba1e0c9cd98d18d2
Daniel <daniel.marques@edu.unifil.br>
2023-03-28 10:32:05 -0300

    annotation usind reflection

--- ProjetosTeste/mytests/core/src/main/java/org/example/core/model/Main.java
+++ ProjetosTeste/mytests/core/src/main/java/org/example/core/model/Main.java
@@ -1,7 +1,14 @@
 package org.example.core.model;
 
+import java.lang.reflect.Field;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IllegalAccessException {
+       Person person = new Person("Daniel");
+
+      for(Field field : person.getClass().getDeclaredFields()){
+          if(field.isAnnotationPresent(ValidName.class)) System.out.println(field.get(person));
+      }
+
     }
 }
--- ProjetosTeste/mytests/core/src/main/java/org/example/core/model/Person.java
+++ ProjetosTeste/mytests/core/src/main/java/org/example/core/model/Person.java
@@ -1,8 +1,12 @@
 package org.example.core.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Person {
+
+    @ValidName
     public String name;
 }
